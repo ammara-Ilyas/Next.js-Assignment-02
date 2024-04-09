6175e52f30b958455ceb5aaa97160528d3050957
Your Name <zeeshannoor7831@gmail.com>
2024-04-09 16:59:30 +0500

    handle Height and side image size

--- portfolio/src/app/components/sections/Hero.tsx
+++ portfolio/src/app/components/sections/Hero.tsx
@@ -4,7 +4,7 @@ import heroImg from "/public/hero-poster.png";
 import Button from "../widgets/Button";
 function Hero() {
   return (
-    <section className="flex flex-wrap flex-col sm:flex-row  w-full justify-center items-center bg-rose-300 gap-3 pt-7 ">
+    <section className="flex flex-wrap flex-col sm:flex-row  w-full h-[64vh] justify-center items-center bg-rose-300 gap-3 pt-7 ">
       <div className="  font-serif w-3/4  flex justify-evenly gap-4 sm:gap-2 sm:w-5/12  md:w-2/5 lg:w-1/3 flex-col ">
         <p className=" font-serif">UX Designer</p>
         <div className="text-3xl font-bold ">
@@ -15,7 +15,7 @@ function Hero() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam esse,
           maiores sed iure impedit numquam?
         </p>
-        <div className="flex  items-center pb-6 w-4/5 justify-between mt- sm:w-full md:w-10/12 lg:w-4/5 xl:w-3/4">
+        <div className="flex items-center pb-6 w-4/5 justify-between mt- sm:w-full md:w-10/12 lg:w-4/5 xl:w-3/4">
           <Button />
           <button className="mx-4 text-blacks py-2 px-7 ml-3 border-black	 bg-white">
             Portfolio
@@ -23,7 +23,7 @@ function Hero() {
         </div>
       </div>
       <div className="  w-2/3 sm:w-5/12 h-72 lg:w-1/3 ">
-        <Image src={heroImg} alt="hero section" className="h-full" />
+        <Image src={heroImg} alt="hero section" className="w-[550px] h-[428px]" />
       </div>
     </section>
   );
